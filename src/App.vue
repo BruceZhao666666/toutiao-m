cb95bb20bd5ec0992ab42635d03a6c41e5a11a91
JIAN815 <1879417918@qq.com>
2021-09-07 02:48:41 +0800

    项目初始化-请求的封装

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,10 @@
       <van-cell title="单元格" value="内容" />
       <van-cell title="单元格" value="内容" label="描述信息" />
     </van-cell-group>
+
+    <div class="box">HelloWorld</div>
   </div>
+
 </template>
 
 <script>
@@ -25,4 +28,9 @@ export default {
 </script>
 
 <style lang="less">
+.box {
+  width: 750px;
+  height: 64px;
+  background-color: pink;
+}
 </style>
