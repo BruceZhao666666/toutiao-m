563346e9fbe479372b03f0d9fbfcbfdcc3ed0850
JIAN815 <1879417918@qq.com>
2021-09-08 00:34:04 +0800

    个人中心

--- src/api/user.js
+++ src/api/user.js
@@ -11,9 +11,22 @@ export const login = data => {
   })
 }
 
+/**
+ * 获取手机验证码
+ */
 export const sendSms = mobile => {
   return request({
     method: 'GET',
     url: `/app/v1_0/sms/codes/${mobile}`
+  })
+}
+
+/**
+ * 获取用户自己的信息
+ */
+export const getUserInfo = () => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/user'
   })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,6 +2,7 @@
  * 请求模块
  */
 import axios from 'axios'
+import store from '@/store'
 
 const request = axios.create({
   // 接口的基准路径
@@ -9,6 +10,20 @@ const request = axios.create({
 })
 
 // 请求拦截器
+request.interceptors.request.use(function (config) {
+  // 请求发起会经过
+  // config：本次请求的配置对象
+  const { user } = store.state
+  if (user && user.token) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+
+  // 务必要返回config配置对象
+  return config
+}, function (error) {
+  // 请求出错会进入
+  return Promise.reject(error)
+})
 
 // 响应拦截器
 
