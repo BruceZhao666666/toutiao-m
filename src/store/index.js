c49d98a9e7118a8af5b1cb8c7a92af8d4ab7ba90
JIAN815 <1879417918@qq.com>
2021-09-07 18:49:58 +0800

    登录注册

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItem, setItem } from '@/utils/storage'
 
 Vue.use(Vuex)
 
+const TOKEN_KEY = 'TOUTIAO_USER'
+
 export default new Vuex.Store({
   state: {
+    // 对象，存储当前登录用户信息（token等数据）
+    // user: JSON.parse(window.localStorage.getItem(TOKEN_KEY))
+    user: getItem(TOKEN_KEY)
   },
   mutations: {
+    setUser (state, data) {
+      state.user = data
+
+      // 防止刷新丢失，把数据保存到本地
+      // window.localStorage.setItem(TOKEN_KEY, JSON.stringify(state.user))
+      setItem(TOKEN_KEY, state.user)
+    }
   },
   actions: {
   },
